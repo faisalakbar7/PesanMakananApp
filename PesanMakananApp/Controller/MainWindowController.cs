39b7b163cefc0abe99568108e1f5a7484cda605c
faisalakbar7 <nervegear08@gmail.com>
2021-11-11 08:55:17 +0700

    Add project files.

---
+++ PesanMakananApp/Controller/MainWindowController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+using PesanMakananApp.Model;
+
+
+
+namespace PesanMakananApp.Controller
+
+{
+
+    class MainWindowController
+
+    {
+
+        KeranjangBelanja keranjangBelanja;
+
+
+
+        public MainWindowController(KeranjangBelanja keranjangBelanja)
+
+        {
+
+            this.keranjangBelanja = keranjangBelanja;
+
+        }
+
+
+
+        public void addItem(Item item)
+
+        {
+
+            this.keranjangBelanja.addItem(item);
+
+        }
+
+        public List<Item> getSelectedItems()
+
+        {
+
+            return this.keranjangBelanja.getItems();
+
+        }
+
+
+
+        public void deleteSelectedItem(Item item)
+
+        {
+
+            this.keranjangBelanja.removeItem(item);
+
+        }
+
+
+
+
+
+    }
+
+
+
+}
---
+++ PesanMakananApp/Controller/PenawaranController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+using PesanMakananApp.Model;
+
+
+
+namespace PesanMakananApp.Controller
+
+{
+
+    class PenawaranController
+
+    {
+
+        private List<Item> items;
+
+
+
+        public PenawaranController()
+
+        {
+
+            items = new List<Item>();
+
+        }
+
+
+
+        public void addItem(Item item)
+
+        {
+
+            this.items.Add(item);
+
+        }
+
+
+
+        public List<Item> getItems()
+
+        {
+
+            return this.items;
+
+        }
+
+    }
+
+}
